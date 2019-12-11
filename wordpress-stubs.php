30e47f6ebf7efb195cbe4aa6e641d21049585c76
Paul Walton <paul@denmandigital.com>
2019-12-11 13:27:51 -0800

    Generate stubs for WP 5.2.3

--- wordpress-stubs.php
+++ wordpress-stubs.php
@@ -104540,18 +104540,6 @@ namespace {
     {
     }
     /**
-     * Preview fix for JavaScript bug with foreign languages.
-     *
-     * @since 3.1.0
-     * @access private
-     *
-     * @param array $match Match array from preg_replace_callback.
-     * @return string
-     */
-    function _convert_urlencoded_to_entities($match)
-    {
-    }
-    /**
      * Display the post excerpt.
      *
      * @since 0.71
